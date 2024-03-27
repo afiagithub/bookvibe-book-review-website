6f5969b1b7b3982e6615ab740bcb88bb1523fb10
Afia Rahman <rahmanmou26@gmail.com>
2024-03-27 16:52:08 +0600

    made a separate file for routes

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,46 @@
+import MainLayout from '../Layouts/MainLayout';
+import BookList from '../Pages/BookList';
+import PagesToRead from '../Pages/PagesToRead';
+import Home from '../Pages/Home';
+import BookDetails from '../Pages/BookDetails';
+import ErrorPage from '../Pages/ErrorPage';
+import AboutUs from '../Pages/AboutUs';
+import PricingPage from '../Pages/PricingPage';
+import { createBrowserRouter } from 'react-router-dom';
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+        loader: () => fetch('../BookData.json'),
+      },
+      {
+        path: "/about",
+        element: <AboutUs></AboutUs>,
+      },
+      {
+        path: "/pricing",
+        element: <PricingPage></PricingPage>,
+        loader: () => fetch('../PriceData.json')
+      },
+      {
+        path: "/:id",
+        element: <BookDetails></BookDetails>,
+        loader: () => fetch('../BookData.json'),
+      },
+      {
+        path: "/books",
+        element: <BookList></BookList>,        
+      },
+      {
+        path: "/pages",
+        element: <PagesToRead></PagesToRead>        
+      },
+    ]
+  }  
+]);
--- src/main.jsx
+++ src/main.jsx
@@ -1,53 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import MainLayout from './Layouts/MainLayout';
-import BookList from './Pages/BookList';
-import PagesToRead from './Pages/PagesToRead';
-import Home from './Pages/Home';
-import BookDetails from './Pages/BookDetails';
-import ErrorPage from './Pages/ErrorPage';
+import { RouterProvider} from "react-router-dom";
 import { Toaster } from 'react-hot-toast';
-import AboutUs from './Pages/AboutUs';
-import PricingPage from './Pages/PricingPage';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout></MainLayout>,
-    errorElement: <ErrorPage></ErrorPage>,
-    children: [
-      {
-        path: "/",
-        element: <Home></Home>,
-        loader: () => fetch('../BookData.json'),
-      },
-      {
-        path: "/about",
-        element: <AboutUs></AboutUs>,
-      },
-      {
-        path: "/pricing",
-        element: <PricingPage></PricingPage>,
-        loader: () => fetch('../PriceData.json')
-      },
-      {
-        path: "/:id",
-        element: <BookDetails></BookDetails>,
-        loader: () => fetch('../BookData.json'),
-      },
-      {
-        path: "/books",
-        element: <BookList></BookList>,        
-      },
-      {
-        path: "/pages",
-        element: <PagesToRead></PagesToRead>        
-      },
-    ]
-  }  
-]);
+import { router } from './Routes/Routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
